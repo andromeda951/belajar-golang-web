b72ba337fb66ebd5a36d2f331a2bb0a701233e64
Andromeda <andromeda951@gmail.com>
2023-01-06 21:51:41 +0700

    Membuat Handler

---
+++ handler_test.go
@@ -0,0 +1,23 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(write http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(write, "Hello World")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
