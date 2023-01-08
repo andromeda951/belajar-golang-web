41ec39d0abe691cb0777083fa6aa27fa619aefb0
Andromeda <andromeda951@gmail.com>
2023-01-08 22:29:40 +0700

    Cookie

---
+++ cookie_test.go
@@ -0,0 +1,106 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie := new(http.Cookie)
+	cookie.Name = "X-PZN-Name"
+	cookie.Value = request.URL.Query().Get("name")
+	cookie.Path = "/"
+
+	http.SetCookie(writer, cookie)
+	fmt.Fprint(writer, "Success create cookie")
+
+}
+
+func GetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie, err := request.Cookie("X-PZN-Name")
+	if err != nil {
+		fmt.Fprint(writer, "no cookie")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", cookie.Value)
+	}
+}
+
+func TestCookie(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/set-cookie", SetCookie)
+	mux.HandleFunc("/get-cookie", GetCookie)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSetCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?name=Andromeda", nil)
+	recorder := httptest.NewRecorder()
+
+	SetCookie(recorder, request)
+	cookies := recorder.Result().Cookies()
+
+	for _, cookie := range cookies {
+		fmt.Println(cookie.Name, cookie.Value)
+	}
+}
+
+func TestGetCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	cookie := new(http.Cookie)
+	cookie.Name = "X-PZN-Name"
+	cookie.Value = "Budi"
+	request.AddCookie(cookie)
+
+	recorder := httptest.NewRecorder()
+
+	GetCookie(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+func TestGetCookieNo(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	// cookie := new(http.Cookie)
+	// cookie.Name = "X-PZN-Name"
+	// cookie.Value = "Budi"
+	// request.AddCookie(cookie)
+
+	recorder := httptest.NewRecorder()
+
+	GetCookie(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+func TestGetCookiePath(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	cookie := new(http.Cookie)
+	cookie.Name = "X-PZN-Name"
+	cookie.Value = "Budi"
+	cookie.Path = "/login"
+	request.AddCookie(cookie) // khusus di unit test, cookie akan selalu di add dan dikirim ke server walaupun url request berbeda dengan path cookienya
+
+	recorder := httptest.NewRecorder()
+
+	GetCookie(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
