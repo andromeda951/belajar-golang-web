01ce93868fdf967487f86131d360983011cf5544
Andromeda <andromeda951@gmail.com>
2023-01-08 21:15:51 +0700

    Response Code

---
+++ responese_code_test.go
@@ -0,0 +1,44 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func ResponseCode(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "name is empty")
+	} else {
+		// w.WriteHeader(200) secara defautl ini di set 200 jika sukses
+		fmt.Fprintf(w, "Hi %s", name)
+	}
+}
+
+func TestResponseCodeInvalid(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func TestResponseCodeValid(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?name=Andromeda", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
